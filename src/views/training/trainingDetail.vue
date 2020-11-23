f14e4808290c4f9e965e77c263ff2736fb50e261
liukangqi <liukangqi56@hotmail.com>
2020-11-23 16:02:23 +0800

    feat: 创建培训模块页面文件

---
+++ src/views/training/trainingDetail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="trainingDetail"></div>
+</template>
+
+<script>
+// 培训详情
+export default {
+  name: 'TrainingDetail'
+}
+</script>
+
+<style lang="scss" scoped>
+.trainingDetail {
+}
+</style>
---
+++ src/views/training/trainingEdit.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="trainingEdit"></div>
+</template>
+
+<script>
+// 培训编辑
+export default {
+  name: 'TrainingEdit'
+}
+</script>
+
+<style lang="scss" scoped>
+.trainingEdit {
+}
+</style>
