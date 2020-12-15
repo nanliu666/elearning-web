977a6d0ecbcaf08bffd071d8acead6a30d960512
liukangqi <liukangqi56@hotmail.com>
2020-12-15 14:01:25 +0800

    feat: 新建手工评卷页面文件

---
+++ src/views/examManagement/mark/MarkByPaper.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="mark-by-paper"></div>
+</template>
+
+<script>
+export default {
+  name: 'MarkByPaper'
+}
+</script>
+
+<style lang="scss" scoped>
+.mark-by-paper {
+}
+</style>
