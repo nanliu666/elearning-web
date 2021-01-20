28a097d1f6af5159e604c042e24e130cf8f0bb90
xiangrun <xiangrun.kong@epro.com.cn>
2021-01-20 16:06:28 +0800

    refactor: 重构评卷详情的页面

--- src/views/examManagement/mark/MarkByExaminee.vue
+++ src/views/examManagement/mark/MarkByExaminee.vue
@@ -1,59 +1,6 @@
 <template>
   <div class="mark-by-examinee">
-    <page-header
-      title="评卷详情"
-      show-back
-    />
-    <el-card
-      v-if="!_.isEmpty(examineeAchievementDO)"
-      class="examinee-card"
-    >
-      <div class="heder">
-        <span>{{ examineeAchievementDO.examName }}</span>
-        <el-tag
-          :type="getTag(examineeAchievementDO.status).type"
-          style="margin-left: 10px"
-        >
-          {{ getTag(examineeAchievementDO.status).label }}
-        </el-tag>
-      </div>
-      <ul class="card-ul">
-        <li class="card-li">
-          <span class="li-label">考试名称：</span>
-          <span class="li-value">******</span>
-        </li>
-        <li class="card-li">
-          <span class="li-label">手机号码：</span>
-          <span class="li-value">******</span>
-        </li>
-        <li class="card-li">
-          <span class="li-label">归属组织：</span>
-          <span class="li-value">{{ examineeAchievementDO.dept }}</span>
-        </li>
-        <li class="card-li">
-          <span class="li-label">考试用卷：</span>
-          <span class="li-value">{{ examineeAchievementDO.paperName }}</span>
-        </li>
-        <li class="card-li">
-          <span class="li-label">考试时间：</span>
-          <span class="li-value">
-            <span>{{ examineeAchievementDO.examBeginTime }}</span>
-            <span>~</span>
-            <span>{{ examineeAchievementDO.examEndTime }}</span>
-          </span>
-        </li>
-        <li class="card-li">
-          <span class="li-label">考试用时：</span>
-          <span class="li-value">
-            {{ getExamUseTime() }}
-          </span>
-        </li>
-        <li class="card-li">
-          <span class="li-label">试卷总分：</span>
-          <span class="li-value">{{ examineeAchievementDO.totalScore }}</span>
-        </li>
-      </ul>
-    </el-card>
+    <mark-header-card :data="examineeAchievementDO" />
     <div v-if="!isView">
       <el-card
         v-if="examData.keguan"
@@ -320,14 +267,9 @@ import {
   postSubmitByOne
 } from '@/api/examManage/mark'
 import { mapGetters } from 'vuex'
-import moment from 'moment'
 import QustionPreview from './components/questionPreview'
 import ComEmpty from '@/components/common-empty/empty'
-const STATUS_STATUS = [
-  { value: '3', label: '待评卷', type: 'success' },
-  { value: '4', label: '阅卷中', type: 'danger' },
-  { value: '5', label: '已评卷', type: 'info' }
-]
+import MarkHeaderCard from './components//MarkHeaderCard'
 import { addLine } from '@/util/util'
 import {
   QUESTION_TYPE_MAP,
@@ -341,6 +283,7 @@ import {
 export default {
   name: 'MarkByExaminee',
   components: {
+    MarkHeaderCard,
     QustionPreview,
     ComEmpty
   },
@@ -493,35 +436,6 @@ export default {
 
       return totalScore
     },
-    // 获取考试用时
-    getExamUseTime() {
-      const diffTime = moment(this.examineeAchievementDO.answerEndTime).diff(
-        moment(this.examineeAchievementDO.answerBeginTime),
-        'ms'
-      )
-      return this.createCountdown(diffTime)
-    },
-    /**
-     * 入参：差异时间
-     * 返回：倒计时
-     * 作用：创建一个倒计时
-     */
-    createCountdown(diffTime) {
-      const hoursTime = moment.duration(diffTime).hours()
-      const minutesTime = moment.duration(diffTime).minutes()
-      const secondsTime = moment.duration(diffTime).seconds()
-      const formatHours = `${hoursTime < 10 ? `0${hoursTime}` : hoursTime}`
-      const formatMinutes = `${minutesTime < 10 ? `0${minutesTime}` : minutesTime}`
-      const formatSeconds = `${secondsTime < 10 ? `0${secondsTime}` : secondsTime}`
-      const targetTime = `${formatHours} : ${formatMinutes} : ${formatSeconds}`
-      return targetTime
-    },
-    getTag(status) {
-      return _.find(STATUS_STATUS, (item) => {
-        return item.value === status
-      })
-    },
-    moment,
     async initData() {
       const examData = await getExamineePaperDetail({ id: this.$route.query.id })
       this.handleExamData(examData)
@@ -538,6 +452,7 @@ export default {
         id: this.$route.query.id
       })
       const paperData = await getExamineePaperDetailist(this.queryInfo)
+      // 页面一进来就区分是评卷还是查看卷子
       this.isView = _.get(this.$route, 'query.isView', false)
       if (this.isView) {
         this.getViewData(paperData)
@@ -591,51 +506,6 @@ export default {
 <style lang="scss" scoped>
 .mark-by-examinee {
   position: relative;
-  .examinee-card {
-    .heder {
-      display: flex;
-      align-items: center;
-      font-family: PingFangSC-Medium;
-      font-size: 18px;
-      color: rgba(0, 11, 21, 0.85);
-      font-weight: 550;
-      margin-bottom: 19px;
-    }
-    /deep/ .el-card__body {
-      padding-bottom: 8px;
-    }
-    .card-ul {
-      display: flex;
-      justify-content: space-between;
-      flex-wrap: wrap;
-      &::after {
-        content: '';
-        flex: auto;
-      }
-      .card-li {
-        width: calc(100% / 3);
-        display: flex;
-        align-items: center;
-        margin-bottom: 16px;
-        .li-label {
-          min-width: 84px;
-          font-family: PingFangSC-Regular;
-          font-size: 14px;
-          color: rgba(0, 11, 21, 0.45);
-        }
-        .li-value {
-          flex: 1;
-          font-family: PingFangSC-Regular;
-          font-size: 14px;
-          color: rgba(0, 11, 21, 0.85);
-          max-width: 350px;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          white-space: nowrap;
-        }
-      }
-    }
-  }
   .paper-card {
     margin-top: 16px;
     .card-header {
