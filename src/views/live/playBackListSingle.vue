0126c1cedbd0151a020b9438970495b649840bb0
lianghuajin <289245330@qq.com>
2021-01-25 15:09:23 +0800

    feat: 单个直播列表

--- src/views/live/playBackListSingle.vue
+++ src/views/live/playBackListSingle.vue
@@ -5,30 +5,34 @@
       <span class="tip">直播回放回传到平台需要一段时间，请耐心等待。</span>
     </div>
     <div>
-      <div class="PBLS">
+      <div
+        v-for="item in playBackListData"
+        :key="item.id"
+        class="PBLS"
+      >
         <div class="img">
           <img
-            src="/img/autol.png"
+            :src="item.coverImageUrl ? item.coverImageUrl : '/img/autol.png'"
             alt=""
             width="220"
             height="124"
           />
         </div>
         <div class="text">
-          <h3>直播回放：20200315 设计能力专项提升</h3>
-          <span>直播分类：研发能力中心 > UCD中心</span>
-          <span>讲师：小明</span>
-          <span>直播时间：2020/03/15 08:00:00</span>
+          <h3>直播回放：{{ item.channelName }}</h3>
+          <span>直播分类：{{ item.fullName }}</span>
+          <span>讲师：{{ item.lecturerName }}</span>
+          <span>直播时间：{{ item.startTime }}</span>
         </div>
         <div class="operation">
-          <span @click="recover">恢复</span>
-          <span>发布</span>
-          <span>下载</span>
-          <span>删除</span>
-          <span>下架</span>
+          <span @click="repRecover(item)">恢复</span>
+          <span @click="repRelease(item)">发布</span>
+          <span @click="repDownload(item)">下载</span>
+          <span @click="repDelete(item)">删除</span>
+          <span @click="repOffShelf(item)">下架</span>
         </div>
       </div>
-      <div class="PBLS">
+      <!-- <div class="PBLS">
         <div class="img">
           <img
             src="/img/autol.png"
@@ -71,16 +75,16 @@
           <span>删除</span>
           <span>下架</span>
         </div>
-      </div>
+      </div> -->
     </div>
     <div class="pagePbls">
       <el-pagination
         background
-        :page-sizes="pageObj.pageSizes"
+        :page-sizes="PBLPageObj.pageSizes"
         :page-size="5"
-        :current-page="pageObj.currentPage"
+        :current-page="PBLPageObj.currentPage"
         layout="total, sizes, prev, pager, next, jumper"
-        :total="pageObj.totalNum"
+        :total="PBLPageObj.totalNum"
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
       />
@@ -88,22 +92,22 @@
   </div>
 </template>
 <script>
-import { getCategoryTree } from '@/api/live/editLive'
+import { liveReplayList, setReplayStatus } from '@/api/live/liveReplay'
 export default {
   name: 'PlayBackListSingle',
   data() {
     return {
       playBackListData: [],
-      pageObj: {
+      PBLParmas: {
+        sourceType: '1',
+        livePlanId: this.$route.query.liveId,
+        pageNo: 1,
+        pageSize: 5
+      },
+      PBLPageObj: {
         pageSizes: [5, 10],
         currentPage: 0,
-        totalNum: 0,
-        parmas: {
-          sourceType: '1',
-          livePlanId: this.$router.query.liveId,
-          pageNo: 1,
-          pageSize: 5
-        }
+        totalNum: 0
       }
     }
   },
@@ -111,31 +115,57 @@ export default {
     this.initPlayBackData()
   },
   methods: {
-    recover() {
+    //  <span @click="repRecover">恢复</span>
+    //       <span @click="repRelease">发布</span>
+    //       <span @click="repDownload">下载</span>
+    //       <span @click="repDelete">删除</span>
+    //       <span @click="repOffShelf">下架</span>
+    repRecover(item) {
       // 恢复
+      let sendPar = { videoId: item.id.toString(), lecturerDeleted: '0' }
+      setReplayStatus(sendPar).then(() => {
+        this.$message({
+          message: '操作成功',
+          type: 'success'
+        })
+      })
     },
-
+    repRelease(item) {
+      // 发布
+      let sendPar = { videoId: item.id.toString(), lecturerDeleted: '0' }
+      setReplayStatus(sendPar).then(() => {
+        this.$message({
+          message: '操作成功',
+          type: 'success'
+        })
+      })
+    },
+    // repDownload(item) {
+    //   // 下载
+    // },
+    // repDelete(item) {
+    //   // 删除
+    // },
+    // repOffShelf(item) {
+    //   // 下架
+    // },
     initPlayBackData() {
       // 初始化直播回放列表
-      getCategoryTree(this.pageObj.parmas).then((res) => {
-        let { data, totalNum, totalPage } = res
+      liveReplayList(this.PBLParmas).then((res) => {
+        let { data, totalNum, totalPage } = res.result
         this.playBackListData = data
-        this.pageObj.totalNum = totalNum
-        this.pageObj.totalPage = totalPage
+        this.PBLPageObj.totalNum = totalNum
+        this.PBLPageObj.totalPage = totalPage
       })
-      // getNoticeCenterList(this.parmas).then((res) => {
-      //   let { data, totalNum, totalPage } = res
-      //   this.noticeList = data
-      //   this.totalNum = totalNum
-      //   this.totalPage = totalPage
-      // })
     },
     handleSizeChange(val) {
-      this.pageObj.parmas.pageSize = val
+      // 改变分页回调
+      this.PBLParmas.pageSize = val
       this.initPlayBackData()
     },
     handleCurrentChange(val) {
-      this.pageObj.parmas.pageNo = val
+      // 改变每页显示条数回调
+      this.PBLParmas.pageNo = val
       this.initPlayBackData()
     }
   }
