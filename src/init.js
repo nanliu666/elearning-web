8cd92d640258e2cf38ea5c6269bd9ca75a4c7365
liukangqi <liukangqi56@hotmail.com>
2021-01-27 17:14:06 +0800

    feat: 添加待审批菜单数字添加逻辑，暂时不启用

--- src/init.js
+++ src/init.js
@@ -1,5 +1,35 @@
 import store from '@/store'
 import router, { resetRouter } from '@/router/router'
+import { fulllist as getApprList } from '@/api/approvalCenter/approvalCenter'
+import { filterTree } from '@/util/util'
+
+// 更新菜单数据里待我审批菜单的menuName,添加待审批数量
+export const updateWaitApprCount = async (count) => {
+  const userId = store.getters.userId
+  if (!userId) {
+    return
+  }
+  let totalNum
+  if (!count) {
+    const prarams = { userId, pageNo: 1, pageSize: 1, status: 'Approve' }
+    const res = await getApprList(prarams)
+    totalNum = res.totalNum
+  } else {
+    totalNum = count
+  }
+  const menuAll = _.cloneDeep(store.getters.menuAll)
+  const waitApprMenu = filterTree(
+    menuAll,
+    (menu) => menu.path == '/approvalCenter/center' || menu.menuName == '课程审核',
+    true
+  )[0]
+  if (!waitApprMenu) {
+    return
+  }
+  waitApprMenu.menuName = totalNum ? `课程审核（${totalNum}）` : waitApprMenu.menuName
+  store.commit('SET_MENU_ALL', menuAll)
+}
+
 export default function() {
   if (!store.getters.userId) {
     return
@@ -7,5 +37,6 @@ export default function() {
   store.dispatch('GetUserPrivilege', store.getters.userId).then((menu) => {
     resetRouter()
     router.$avueRouter.formatRoutes(menu, true)
+    // updateWaitApprCount()
   })
 }
