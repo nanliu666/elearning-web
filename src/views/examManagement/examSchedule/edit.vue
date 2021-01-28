ad01f7cfe2edb000c657334ff8c5c27f299b6c60
xiangrun <xiangrun.kong@epro.com.cn>
2021-01-28 17:19:21 +0800

    fix: 删除页面中的上一步的校验

--- src/views/examManagement/examSchedule/edit.vue
+++ src/views/examManagement/examSchedule/edit.vue
@@ -161,7 +161,7 @@ export default {
      *
      * */
     handleNextStep() {
-      this.$refs[REFS_LIST[this.activeStep]].getData().then(() => {
+      this.$refs[REFS_LIST[0]].getData().then(() => {
         this.activeStep = this.activeStep === 1 ? 0 : this.activeStep + 1
       })
     },
