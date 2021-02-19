3fa1e6ff0b6f46432e66c3f2338cbce33f16c1ed
huangjieqiu <617940459@qq.com>
2021-02-19 15:30:46 +0800

    fix: 参数字段改变

--- src/views/live/live-stat-detail.vue
+++ src/views/live/live-stat-detail.vue
@@ -300,7 +300,7 @@ export default {
     }
   },
   activated() {
-    this.getData2params.livePlanId = this.$route.query.livePlanId
+    this.getData2params.livePlanId = this.$route.query.liveId
     this.getData()
   },
   methods: {
@@ -309,7 +309,7 @@ export default {
       this.getTableData()
     },
     getSummary() {
-      getSummary({ livePlanId: this.$route.query.livePlanId }).then((res) => {
+      getSummary({ livePlanId: this.$route.query.liveId }).then((res) => {
         Object.keys(res).forEach((key) => {
           this.data[key] = +res[key]
         })
