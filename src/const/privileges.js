361b42460d9b8a7dfcfcc4b81d1475a1d1a689a9
xiangrun <xiangrun.kong@epro.com.cn>
2021-02-23 10:02:14 +0800

    fix: 知识库附件下载使用弹窗形式

--- src/const/privileges.js
+++ src/const/privileges.js
@@ -132,7 +132,7 @@ export const DELETE_PERMISSION = '/system/permissionConfig/delete' // 删除
 // 角色管理的查看用户的鉴权
 export const ADD_ROLE_USER = '/system/roleUsers/add' // 添加用户
 export const DELETE_ROLE_USER = '/system/roleUsers/delete' // 删除
-// 角色管理的查看用户的鉴权
+// 流程管理的审批流程的鉴权
 export const ADD_APPROVAL = '/apprProcess/approvalList/add' // 创建审批流程
 export const STOP_APPROVAL = '/apprProcess/approvalList/stop' // 启用/停用
 export const EIDT_APPROVAL = '/apprProcess/approvalList/eidt' // 编辑
