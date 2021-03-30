98be324330d594bea0be84125b0fbc2be1edfe3b
xiangrun <xiangrun.kong@epro.com.cn>
2021-03-30 10:02:21 +0800

    fix: 隐藏fixedTime的时候必填校验关闭

--- src/views/examManagement/examSchedule/components/examInfo.vue
+++ src/views/examManagement/examSchedule/components/examInfo.vue
@@ -707,6 +707,8 @@ export default {
       handler(value) {
         // 1隐藏， 2显示
         _.set(_.find(this.columns, { prop: 'fixedTime' }), 'isVisible', value === 2)
+        // 隐藏的时候必填校验关闭
+        _.set(_.find(this.columns, { prop: 'fixedTime' }), 'required', value === 2)
       },
       deep: true,
       immediate: true
