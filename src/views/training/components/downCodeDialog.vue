39e525bf993dea2be73557d8f5f5d2f6157abc70
huangjieqiu <617940459@qq.com>
2021-04-01 13:49:50 +0800

    fix: bug修复

--- src/views/training/components/downCodeDialog.vue
+++ src/views/training/components/downCodeDialog.vue
@@ -8,9 +8,17 @@
     class="down-code-dialog"
     @close="onClose"
   >
-    <div slot="title" class="dialog-header">
-      <el-row type="flex" justify="space-between">
-        <el-col :span="6"> 签到二维码下载 </el-col>
+    <div
+      slot="title"
+      class="dialog-header"
+    >
+      <el-row
+        type="flex"
+        justify="space-between"
+      >
+        <el-col :span="6">
+          签到二维码下载
+        </el-col>
         <el-col :span="2">
           <el-button
             type="text"
@@ -26,29 +34,52 @@
     </div>
 
     <el-table :data="data">
-      <el-table-column align="center" label="课程名称">
+      <el-table-column
+        align="center"
+        label="课程名称"
+      >
         <template slot-scope="scope">
           <div>
             {{ scope.row.todoName }}
           </div>
         </template>
       </el-table-column>
-      <el-table-column prop="todoType" align="center" label="课程类型">
+      <el-table-column
+        prop="todoType"
+        align="center"
+        label="课程类型"
+      >
         <template slot-scope="scope">
-          {{ scope.row.todoType == "course" ? "面授课程" : "线下活动" }}
+          {{ scope.row.todoType == 'course' ? '面授课程' : '线下活动' }}
         </template>
       </el-table-column>
-      <el-table-column prop="status" align="center" label="状态">
+      <el-table-column
+        prop="status"
+        align="center"
+        label="状态"
+      >
         <template slot-scope="scope">
-          {{
-            scope.row.status == 1 ? "未开始" : scope.row.status == 2 ? "进行中" : "已结束"
-          }}
+          {{ scope.row.status == 1 ? '未开始' : scope.row.status == 2 ? '进行中' : '已结束' }}
         </template>
       </el-table-column>
-      <el-table-column prop="todoTime" align="center" label="授课时间"> </el-table-column>
-      <el-table-column prop="date" align="center" label="操作">
+      <el-table-column
+        prop="todoTime"
+        align="center"
+        label="授课时间"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        align="center"
+        label="操作"
+      >
         <template slot-scope="scope">
-          <el-button type="text" @click="downCodeImg(scope.row)"> 下载二维码 </el-button>
+          <el-button
+            type="text"
+            @click="downCodeImg(scope.row)"
+          >
+            下载二维码
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -56,88 +87,88 @@
 </template>
 
 <script>
-import JsZip from "jszip";
-import { saveAs } from "file-saver";
-import QRCode from "qrcodejs2";
-import { getQrcode } from "@/api/learnArrange";
-import { backBaseUrl } from "@/config/env";
+import JsZip from 'jszip'
+import { saveAs } from 'file-saver'
+import QRCode from 'qrcodejs2'
+import { getQrcode } from '@/api/learnArrange'
+import { backBaseUrl } from '@/config/env'
 
 if (!HTMLCanvasElement.prototype.toBlob) {
-  Object.defineProperty(HTMLCanvasElement.prototype, "toBlob", {
-    value: function (callback, type, quality) {
-      var dataURL = this.toDataURL(type, quality).split(",")[1];
-      setTimeout(function () {
+  Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
+    value: function(callback, type, quality) {
+      var dataURL = this.toDataURL(type, quality).split(',')[1]
+      setTimeout(function() {
         var binStr = atob(dataURL),
           len = binStr.length,
-          arr = new Uint8Array(len);
+          arr = new Uint8Array(len)
 
         for (var i = 0; i < len; i++) {
-          arr[i] = binStr.charCodeAt(i);
+          arr[i] = binStr.charCodeAt(i)
         }
 
-        callback(new Blob([arr], { type: type || "image/png" }));
-      });
-    },
-  });
+        callback(new Blob([arr], { type: type || 'image/png' }))
+      })
+    }
+  })
 }
 
-const CODE_HEIGHT = 100;
-const CODE_WIDTH = 100;
-const FOLDER_NAME = "签到二维码";
+const CODE_HEIGHT = 100
+const CODE_WIDTH = 100
+const FOLDER_NAME = '签到二维码'
 export default {
   props: {
     dialogVisibe: {
       type: Boolean,
-      default: false,
+      default: false
     },
     trainId: {
       type: String,
-      default: "",
-    },
+      default: ''
+    }
   },
   data() {
     return {
       downloadLoading: false,
       data: [],
-      total: 0,
-    };
+      total: 0
+    }
   },
   watch: {
     trainId() {
-      this.getData();
-    },
+      this.getData()
+    }
   },
   methods: {
     downCodeImg(row) {
-      let { todoName, offlineTodoId } = row;
-      if (!todoName.trim().length) todoName = "签到二维码";
+      let { todoName, offlineTodoId } = row
+      if (!todoName.trim().length) todoName = '签到二维码'
       const params = {
         trainId: this.trainId,
-        offlineTodoId,
-      };
-      const codeContainer = document.createElement("div");
+        offlineTodoId
+      }
+      const codeContainer = document.createElement('div')
       var qrcode = new QRCode(codeContainer, {
         width: CODE_WIDTH,
-        height: CODE_HEIGHT,
-      });
-      let url = backBaseUrl;
-      let baseURL = (url += "/mobile/#/pages/signin/index");
+        height: CODE_HEIGHT
+      })
+      let url = backBaseUrl
+      let baseURL = (url += '/mobile/#/pages/signin/index')
 
-      let p = "";
+      let p = ''
       Object.keys(params).forEach((key) => {
-        p += "&" + key + "=" + params[key];
-      });
+        p += '&' + key + '=' + params[key]
+      })
 
-      baseURL += "?" + p.slice(1);
-      qrcode.makeCode(baseURL);
-      var canvas = qrcode._el.children[0];
-      var data = canvas.toDataURL().replace("image/png", "image/octet-stream;"); //获取二维码值，并修改响应头部。
-      var saveLink = document.createElementNS("http://www.w3.org/1999/xhtml", "a");
-      saveLink.href = data;
-      saveLink.download = `${todoName}.png`;
-      var event = document.createEvent("MouseEvents");
+      baseURL += '?' + p.slice(1)
+      qrcode.makeCode(baseURL)
+      var canvas = qrcode._el.children[0]
+      var data = canvas.toDataURL().replace('image/png', 'image/octet-stream;') //获取二维码值，并修改响应头部。
+      var saveLink = document.createElementNS('http://www.w3.org/1999/xhtml', 'a')
+      saveLink.href = data
+      saveLink.download = `${todoName}.png`
+      var event = document.createEvent('MouseEvents')
       event.initMouseEvent(
-        "click",
+        'click',
         true,
         false,
         window,
@@ -152,56 +183,56 @@ export default {
         false,
         0,
         null
-      );
-      saveLink.dispatchEvent(event);
+      )
+      saveLink.dispatchEvent(event)
     },
     batchDownload() {
-      this.downloadLoading = true;
-      const codeContainer = document.createElement("div");
+      this.downloadLoading = true
+      const codeContainer = document.createElement('div')
       var qrcode = new QRCode(codeContainer, {
         width: CODE_WIDTH,
-        height: CODE_HEIGHT,
-      });
-      var zip = new JsZip();
-      var promises = [];
+        height: CODE_HEIGHT
+      })
+      var zip = new JsZip()
+      var promises = []
       this.data.forEach((item) => {
-        let { todoName, offlineTodoId } = item;
-        if (!todoName.trim().length) todoName = "签到二维码";
+        let { todoName, offlineTodoId } = item
+        if (!todoName.trim().length) todoName = '签到二维码'
         const params = {
           trainId: this.trainId,
-          offlineTodoId,
-        };
-        qrcode.clear();
-        qrcode.makeCode(JSON.stringify(params));
-        var canvas = qrcode._el.children[0];
-        var imgFolder = zip.folder(FOLDER_NAME);
+          offlineTodoId
+        }
+        qrcode.clear()
+        qrcode.makeCode(JSON.stringify(params))
+        var canvas = qrcode._el.children[0]
+        var imgFolder = zip.folder(FOLDER_NAME)
         promises.push(
           new Promise((resolve) => {
-            canvas.toBlob(function (blob) {
-              imgFolder.file(todoName + ".png", blob);
-              resolve();
-            });
+            canvas.toBlob(function(blob) {
+              imgFolder.file(todoName + '.png', blob)
+              resolve()
+            })
           })
-        );
-      });
+        )
+      })
 
       Promise.all(promises).then(() => {
-        zip.generateAsync({ type: "blob" }).then(function (content) {
-          saveAs(content, `${FOLDER_NAME}.zip`);
-        });
-        this.downloadLoading = false;
-      });
+        zip.generateAsync({ type: 'blob' }).then(function(content) {
+          saveAs(content, `${FOLDER_NAME}.zip`)
+        })
+        this.downloadLoading = false
+      })
     },
     getData() {
       getQrcode({ trainId: this.trainId }).then((res) => {
-        this.data = res;
-      });
+        this.data = res
+      })
     },
     onClose() {
-      this.$emit("update:dialogVisibe", false);
-    },
-  },
-};
+      this.$emit('update:dialogVisibe', false)
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
