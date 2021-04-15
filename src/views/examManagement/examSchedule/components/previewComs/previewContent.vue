d05b593be38dc204d25cfd9fa3c789b8ec75bea8
xiangrun <xiangrun.kong@epro.com.cn>
2021-04-15 15:20:19 +0800

    refactor: 重构预览试卷

---
+++ src/views/examManagement/examSchedule/components/previewComs/previewContent.vue
@@ -0,0 +1,177 @@
+<template>
+  <div
+    ref="testPaper"
+    v-loading="loading"
+    class="preview-container pdf-screen"
+  >
+    <div class="preview-title-box">
+      <div class="preview-title">
+        {{ paperData.name }}
+        <span v-if="$route.query.previewSort">（{{ $route.query.previewSort }}）</span>
+      </div>
+      <div
+        v-if="paperData.totalScore"
+        class="preview-subhead"
+      >
+        试卷总分：{{ paperData.totalScore }}分
+      </div>
+    </div>
+    <ul>
+      <li
+        v-for="(item, index) in paperData.questions"
+        :key="index"
+        class="main-question-li"
+      >
+        <div class="li__title">
+          <span>{{ (index + 1) | number2zhcn }}、</span>
+          <span>{{ item[0].type | typeFilter }}</span>
+          <span>（共{{ _.size(item) }}题, 共{{ getItemTotalScore(item) }}分）</span>
+        </div>
+        <div style="margin-bottom: 10px; margin-top: 4px;color: #bfc2c4">
+          {{ _.get(item, '[0]title', '') }}
+        </div>
+        <div
+          v-for="(sonitem, sonindex) in item"
+          :key="sonindex"
+          class="sub-question-li"
+        >
+          <div v-if="QUESTION_TYPE_GROUP !== sonitem.type">
+            <span>{{ sonindex + 1 }}.</span>
+            <QustionPreview
+              :data="sonitem"
+              :has-answer="hasAnswer"
+            />
+          </div>
+          <div v-else>
+            <span>{{ sonindex + 1 }}</span>
+            <span>
+              <span v-html="getHTML(sonitem.content)" />
+              <ul class="sub__ul">
+                <li
+                  v-for="(paperItem, paperIndex) in sonitem.subQuestions"
+                  :key="paperIndex"
+                  class="sub__li"
+                >
+                  <span>({{ paperIndex + 1 }}).</span>
+                  <QustionPreview
+                    :data="paperItem"
+                    :has-answer="hasAnswer"
+                  />
+                </li>
+              </ul>
+            </span>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import QustionPreview from './questionPreview'
+import {
+  QUESTION_TYPE_MAP,
+  QUESTION_TYPE_MULTIPLE,
+  QUESTION_TYPE_SINGLE,
+  QUESTION_TYPE_JUDGE,
+  QUESTION_TYPE_SHOER,
+  QUESTION_TYPE_BLANK,
+  QUESTION_TYPE_GROUP
+} from '@/const/examMange'
+import { addLine } from '@/util/util'
+const nzhcn = require('nzh/cn')
+export default {
+  name: 'PreviewContent',
+  components: {
+    QustionPreview
+  },
+  filters: {
+    typeFilter(data) {
+      return QUESTION_TYPE_MAP[data]
+    },
+    number2zhcn(index) {
+      return nzhcn.encodeS(index)
+    }
+  },
+  props: {
+    hasAnswer: {
+      type: Boolean,
+      default: false
+    },
+    paperData: {
+      type: [Array, Object],
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      loading: false
+    }
+  },
+  computed: {
+    QUESTION_TYPE_MULTIPLE: () => QUESTION_TYPE_MULTIPLE,
+    QUESTION_TYPE_SINGLE: () => QUESTION_TYPE_SINGLE,
+    QUESTION_TYPE_JUDGE: () => QUESTION_TYPE_JUDGE,
+    QUESTION_TYPE_BLANK: () => QUESTION_TYPE_BLANK,
+    QUESTION_TYPE_SHOER: () => QUESTION_TYPE_SHOER,
+    QUESTION_TYPE_MAP: () => QUESTION_TYPE_MAP,
+    QUESTION_TYPE_GROUP: () => QUESTION_TYPE_GROUP
+  },
+
+  methods: {
+    addScore(args) {
+      return args.reduce((prev, curr) => {
+        return prev + curr
+      })
+    },
+    getItemTotalScore(data) {
+      const scoreList = _.map(data, (item) => {
+        return item.score
+      })
+      const totalScore = this.addScore(scoreList)
+      return totalScore
+    },
+    getHTML(content) {
+      return addLine(content)
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+.preview-title-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  .preview-title {
+    font-weight: 550;
+    font-size: 18px;
+    margin-bottom: 10px;
+  }
+  .preview-subhead {
+    font-size: 12px;
+    color: #9c9e9c;
+  }
+}
+.main-question-li {
+  padding: 40px 0;
+  border-bottom: 1px solid #e4e7e9;
+  .li__title {
+    font-family: PingFangSC-Medium;
+    font-size: 16px;
+    color: rgba(0, 11, 21, 0.85);
+    font-weight: 550;
+    margin-bottom: 4px;
+  }
+  .sub-question-li {
+    margin-bottom: 40px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+    .sub__ul {
+      .sub__li {
+        margin-bottom: 20px;
+      }
+    }
+  }
+}
+</style>
--- src/views/examManagement/examSchedule/preview.vue
+++ src/views/examManagement/examSchedule/preview.vue
@@ -4,125 +4,49 @@
       title="预览试卷"
       show-back
     >
-      <el-button
-        v-if="$route.query.paperType !== 'manual' && !$route.query.isManaged"
-        slot="rightMenu"
-        size="medium"
-        @click="deletePreview"
-      >
-        删除
-      </el-button>
+      <div slot="rightMenu">
+        <el-button
+          v-if="$route.query.paperType !== 'manual' && !$route.query.isManaged"
+          size="medium"
+          @click="deletePreview"
+        >
+          删除
+        </el-button>
+        <el-button
+          size="medium"
+          @click="createTestpaper"
+        >
+          生成笔试试卷
+        </el-button>
+      </div>
     </page-header>
     <basic-container>
-      <div
-        v-loading="loading"
-        class="preview-container"
-      >
-        <div class="preview-title-box">
-          <div class="preview-title">
-            {{ paperData.name }}
-            <span v-if="$route.query.previewSort">（{{ $route.query.previewSort }}）</span>
-          </div>
-          <div
-            v-if="paperData.totalScore"
-            class="preview-subhead"
-          >
-            试卷总分：{{ paperData.totalScore }}分
-          </div>
-        </div>
-        <ul>
-          <li
-            v-for="(item, index) in paperData.questions"
-            :key="index"
-            class="main-question-li"
-          >
-            <div style="font-szie: 16px;font-weight: 550">
-              <span>{{ (index + 1) | number2zhcn }}、</span>
-              <span>{{ item[0].type | typeFilter }}</span>
-              <span>（共{{ _.size(item) }}题, 共{{ getItemTotalScore(item) }}分）</span>
-            </div>
-            <div style="margin-bottom: 10px; margin-top: 4px;color: #bfc2c4">
-              {{ _.get(item, '[0]title', '') }}
-            </div>
-            <div
-              v-for="(sonitem, sonindex) in item"
-              :key="sonindex"
-              class="sub-question-li"
-            >
-              <div v-if="QUESTION_TYPE_GROUP !== sonitem.type">
-                <span>{{ sonindex + 1 }}.</span>
-                <QustionPreview
-                  :data="sonitem"
-                  :is-preview="true"
-                />
-              </div>
-              <div v-else>
-                <span>{{ sonindex + 1 }}</span>
-                <span>
-                  <span v-html="getHTML(sonitem.content)" />
-                  <ul>
-                    <li
-                      v-for="(paperItem, paperIndex) in sonitem.subQuestions"
-                      :key="paperIndex"
-                      class=""
-                    >
-                      <span>({{ paperIndex + 1 }}).</span>
-                      <QustionPreview
-                        :data="paperItem"
-                        :is-preview="true"
-                      />
-                    </li>
-                  </ul>
-                </span>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
+      <preview-content
+        :has-answer="true"
+        :paper-data="paperData"
+      />
     </basic-container>
+    <preview-dialog
+      :visible.sync="previewDialogVisible"
+      :paper-data="paperData"
+    />
   </div>
 </template>
 
 <script>
-import QustionPreview from '../question/questionPreview'
+import PreviewContent from './components/previewComs/previewContent'
+import PreviewDialog from './components/previewComs/previewDialog'
 import { getManualPreview, delExamPreview, createRandomPaper } from '@/api/examManage/schedule'
-import {
-  QUESTION_TYPE_MAP,
-  QUESTION_TYPE_MULTIPLE,
-  QUESTION_TYPE_SINGLE,
-  QUESTION_TYPE_JUDGE,
-  QUESTION_TYPE_SHOER,
-  QUESTION_TYPE_BLANK,
-  QUESTION_TYPE_GROUP
-} from '@/const/examMange'
-import { addLine } from '@/util/util'
-const nzhcn = require('nzh/cn')
 export default {
-  filters: {
-    typeFilter(data) {
-      return QUESTION_TYPE_MAP[data]
-    },
-    number2zhcn(index) {
-      return nzhcn.encodeS(index)
-    }
-  },
   components: {
-    QustionPreview
+    PreviewDialog,
+    PreviewContent
   },
   data() {
     return {
-      loading: false,
+      previewDialogVisible: false,
       paperData: []
     }
-  },
-  computed: {
-    QUESTION_TYPE_MULTIPLE: () => QUESTION_TYPE_MULTIPLE,
-    QUESTION_TYPE_SINGLE: () => QUESTION_TYPE_SINGLE,
-    QUESTION_TYPE_JUDGE: () => QUESTION_TYPE_JUDGE,
-    QUESTION_TYPE_BLANK: () => QUESTION_TYPE_BLANK,
-    QUESTION_TYPE_SHOER: () => QUESTION_TYPE_SHOER,
-    QUESTION_TYPE_MAP: () => QUESTION_TYPE_MAP,
-    QUESTION_TYPE_GROUP: () => QUESTION_TYPE_GROUP
   },
   beforeRouteLeave(to, from, next) {
     this.$store.commit('DEL_TAG', this.$store.state.tags.tag)
@@ -132,26 +56,26 @@ export default {
     this.initData()
   },
   methods: {
-    addScore(args) {
-      return args.reduce((prev, curr) => {
-        return prev + curr
-      })
-    },
-    getItemTotalScore(data) {
-      const scoreList = _.map(data, (item) => {
-        return item.score
-      })
-      const totalScore = this.addScore(scoreList)
-      return totalScore
-    },
-    getHTML(content) {
-      return addLine(content)
+    /**
+     * 打开笔试试卷窗口
+     */
+    createTestpaper() {
+      this.previewDialogVisible = true
     },
     deletePreview() {
       delExamPreview({ id: this.$route.query.paperId }).then(() => {
         this.$message.success('删除成功')
         this.loadTableData('del')
       })
+    },
+    handleData(data) {
+      return _.chain(data)
+        .groupBy('parentSort')
+        .sortBy('parentSort')
+        .map((item) => {
+          return _.sortBy(item, 'sort')
+        })
+        .value()
     },
     initData() {
       this.paperData = []
@@ -173,15 +97,6 @@ export default {
         .finally(() => {
           this.loading = false
         })
-    },
-    handleData(data) {
-      return _.chain(data)
-        .groupBy('parentSort')
-        .sortBy('parentSort')
-        .map((item) => {
-          return _.sortBy(item, 'sort')
-        })
-        .value()
     }
   }
 }
@@ -191,30 +106,5 @@ export default {
 .preview-container {
   padding: 60px 80px;
   min-height: 75vh;
-  .preview-title-box {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    .preview-title {
-      font-weight: 550;
-      font-size: 18px;
-      margin-bottom: 10px;
-    }
-    .preview-subhead {
-      font-size: 12px;
-      color: #9c9e9c;
-    }
-  }
-  .main-question-li {
-    padding: 40px 0;
-    border-bottom: 1px solid #e4e7e9;
-    .sub-question-li {
-      margin-bottom: 40px;
-      &:last-child {
-        margin-bottom: 0;
-      }
-    }
-  }
 }
 </style>
