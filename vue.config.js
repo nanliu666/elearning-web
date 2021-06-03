9c1d9a4f349a8c31073c8bcf27830e74050e2211
yuyong <dreamsyuyong@163.com>
2021-06-03 14:54:23 +0800

    fix: test

--- vue.config.js
+++ vue.config.js
@@ -104,7 +104,7 @@ module.exports = {
         // target: 'http://apidev.epro.com.cn/',
 
         // 测试环境
-        // target: 'http://139.159.141.248:8081',
+         target: 'http://139.159.141.248:8081',
 
         // 深圳本地开发环境
         // target: 'http://172.16.4.55:8081',
@@ -113,7 +113,7 @@ module.exports = {
         // target: 'http://192.168.40.134',
 
         // 贺磊IP
-        target: 'http://172.16.4.120',
+        //target: 'http://172.16.4.120',
 
         // 殷云
         // target: 'http://172.16.4.237',
