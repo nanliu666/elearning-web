4063f12182d6cab4c89a705dfa0dc6dba97c4f82
lianghuajin <1034572928@qq.com>
2021-07-06 15:07:30 +0800

    feat: 徐工公共同步到泽汇

--- src/views/course/establishCourse.vue
+++ src/views/course/establishCourse.vue
@@ -245,7 +245,7 @@
           </el-col>
         </el-row>
         <!-- 第五行 -->
-        <el-row>
+        <!-- <el-row>
           <el-col :span="11">
             <el-form-item label="课程认证等级">
               <el-select v-model="ruleForm.grade">
@@ -281,10 +281,10 @@
               </el-radio-group>
             </el-form-item>
           </el-col>
-        </el-row>
+        </el-row> -->
 
         <!-- 第六行 -->
-        <el-row>
+        <!-- <el-row>
           <el-col :span="11">
             <el-form-item label="开发费用">
               <el-input-number
@@ -314,7 +314,7 @@
               </el-select>
             </el-form-item>
           </el-col>
-        </el-row>
+        </el-row> -->
         <!-- 第七行 -->
         <el-row>
           <el-col :span="11">
